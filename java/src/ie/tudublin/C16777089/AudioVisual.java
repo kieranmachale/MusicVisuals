8580e8b4d4ba0e201d5d30f18a60cfaa0060a1aa
user <user@users-MacBook-Pro.local>
2020-05-14 05:14:11 +0100

    adding my Audio Visual class

---
+++ java/src/ie/tudublin/C16777089/AudioVisual.java
@@ -0,0 +1,10 @@
+package ie.tudublin.C16777089;
+
+import ie.tudublin.Visual;
+import processing.core.PApplet;
+import ddf.minim.*;
+import ddf.minim.analysis.FFT;
+
+public class AudioVisual extends Visual{
+    
+}
